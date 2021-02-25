49baa2bb50df8a362abaafaaf878c76be0bdf992
Cedric Morreau <cedricmorreau@gmail.com>
2021-02-25 10:04:53 +0100

    gatsby/ build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/.cache/dev-404-page.js"))),
   "component---src-pages-components-assets-github-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/github-icon.js"))),
   "component---src-pages-components-assets-ig-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/ig-icon.js"))),
   "component---src-pages-components-assets-menu-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/menu-icon.js"))),
@@ -13,7 +12,6 @@ exports.components = {
   "component---src-pages-components-header-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/header.js"))),
   "component---src-pages-components-layout-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/layout.js"))),
   "component---src-pages-components-menu-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/menu.js"))),
-  "component---src-pages-components-week-1-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/week1.js"))),
   "component---src-pages-components-welcome-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/welcome.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/index.js")))
 }
---
+++ public/component---src-pages-components-footer-js-17a2b75a0d03379fab43.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{qFMz:function(e,t,n){"use strict";n.r(t);var r=n("q1tI"),a=n.n(r);t.default=function(){return a.a.createElement("footer",null,a.a.createElement("div",{className:"footer_wrapper"},a.a.createElement("p",null,"footer (of iets dergelijks)")))}}}]);
+//# sourceMappingURL=component---src-pages-components-footer-js-17a2b75a0d03379fab43.js.map
