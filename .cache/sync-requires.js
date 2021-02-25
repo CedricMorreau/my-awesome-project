94c2247a3c211b212f0c9093f1ca800586849895
Cedric Morreau <cedricmorreau@gmail.com>
2021-02-25 11:20:43 +0100

    gatsby/ live #1

---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-components-assets-github-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/github-icon.js"))),
+  "component---src-pages-components-assets-ig-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/ig-icon.js"))),
+  "component---src-pages-components-assets-menu-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/menu-icon.js"))),
+  "component---src-pages-components-footer-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/footer.js"))),
+  "component---src-pages-components-header-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/header.js"))),
+  "component---src-pages-components-layout-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/layout.js"))),
+  "component---src-pages-components-menu-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/menu.js"))),
+  "component---src-pages-components-welcome-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/welcome.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/index.js")))
+}
+
