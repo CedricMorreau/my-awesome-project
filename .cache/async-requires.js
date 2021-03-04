fdb8f28c095a8b3a593735a72e23e158fdb335bb
Cedric Morreau <cedricmorreau@gmail.com>
2021-03-04 09:17:57 +0100

    build fix + gitignore

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,14 +2,12 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-components-assets-github-icon-js": () => import("./../../../src/pages/components/assets/github-icon.js" /* webpackChunkName: "component---src-pages-components-assets-github-icon-js" */),
   "component---src-pages-components-assets-ig-icon-js": () => import("./../../../src/pages/components/assets/ig-icon.js" /* webpackChunkName: "component---src-pages-components-assets-ig-icon-js" */),
   "component---src-pages-components-assets-menu-icon-js": () => import("./../../../src/pages/components/assets/menu-icon.js" /* webpackChunkName: "component---src-pages-components-assets-menu-icon-js" */),
   "component---src-pages-components-footer-js": () => import("./../../../src/pages/components/footer.js" /* webpackChunkName: "component---src-pages-components-footer-js" */),
   "component---src-pages-components-header-js": () => import("./../../../src/pages/components/header.js" /* webpackChunkName: "component---src-pages-components-header-js" */),
   "component---src-pages-components-layout-js": () => import("./../../../src/pages/components/layout.js" /* webpackChunkName: "component---src-pages-components-layout-js" */),
-  "component---src-pages-components-lesweken-week-1-js": () => import("./../../../src/pages/components/lesweken/week1.js" /* webpackChunkName: "component---src-pages-components-lesweken-week-1-js" */),
   "component---src-pages-components-menu-js": () => import("./../../../src/pages/components/menu.js" /* webpackChunkName: "component---src-pages-components-menu-js" */),
   "component---src-pages-components-welcome-js": () => import("./../../../src/pages/components/welcome.js" /* webpackChunkName: "component---src-pages-components-welcome-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,14 +5,12 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/.cache/dev-404-page.js"))),
   "component---src-pages-components-assets-github-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/github-icon.js"))),
   "component---src-pages-components-assets-ig-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/ig-icon.js"))),
   "component---src-pages-components-assets-menu-icon-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/assets/menu-icon.js"))),
   "component---src-pages-components-footer-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/footer.js"))),
   "component---src-pages-components-header-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/header.js"))),
   "component---src-pages-components-layout-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/layout.js"))),
-  "component---src-pages-components-lesweken-week-1-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/lesweken/week1.js"))),
   "component---src-pages-components-menu-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/menu.js"))),
   "component---src-pages-components-welcome-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/components/welcome.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/cedricmorreau/Documents/GitHub/my-first-repo/src/pages/index.js")))
